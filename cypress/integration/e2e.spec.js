4dbe40a557d2b1ebf1823647ac24a4d6b640f53f
Janaina Borcate <96551202+Borcate@users.noreply.github.com>
2022-10-12 14:39:36 -0300

    Update e2e.spec.js

--- cypress/integration/e2e.spec.js
+++ cypress/integration/e2e.spec.js
@@ -19,20 +19,25 @@ context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
     });
 
     it('Deve selecionar produtos e validar compra', () => {
-        cy.pedidos_produtos('Aether Gym Pant', '36', 'Blue', 1)
+       // cy.pedidos_produtos('Aether Gym Pant', '36', 'Blue', 1)
 
-        cy.get('#primary-menu > .menu-item-629 > a').click()
-        cy.pedidos_produtos('Ajax Full-Zip Sweatshirt', 'S', 'Red', 1)
+       // cy.get('#primary-menu > .menu-item-629 > a').click()
+       // cy.pedidos_produtos('Ajax Full-Zip Sweatshirt', 'S', 'Red', 1)
 
-        cy.get('#primary-menu > .menu-item-629 > a').click()
-        cy.pedidos_produtos('Atlas Fitness Tank', 'Blue','L', 1)
+       // cy.get('#primary-menu > .menu-item-629 > a').click()
+        //cy.pedidos_produtos('Atlas Fitness Tank', 'Blue','L', 1)
 
         cy.get('#primary-menu > .menu-item-629 > a').click()
         cy.get(':nth-child(2) > .page-numbers').click()
         cy.pedidos_produtos('Augusta Pullover Jacket', 'M', 'Red', 1)
         
-        cy.get('.dropdown-toggle > .mini-cart-items').click()
-        cy.get('#cart > .dropdown-menu > .widget_shopping_cart_content > .mini_cart_content > .mini_cart_inner > .mcart-border > .buttons > .checkout').click()
+        //cy.get('.dropdown-toggle > .mini-cart-items').click()
+        //cy.get('#cart > .dropdown-menu > .widget_shopping_cart_content > .mini_cart_content > .mini_cart_inner > .mcart-border > .buttons > .checkout').click()
+
+        //botão ver carrinho
+        cy.get('.woocommerce-message > .button').click()
+        //botão concluir compra
+        cy.get('.checkout-button').click()
 
         cy.get('.showlogin').click()
         cy.get('#username').type(perfil.usuario)
